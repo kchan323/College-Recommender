5ec70fa2f32cb486cac67883fdca4aa4651bfd0a
kchan323 <katelin.chan@gmail.com>
2021-05-16 12:40:03 -0400

    accessing inputs

--- proj/main.py
+++ proj/main.py
@@ -7,11 +7,18 @@ result1 = 'hi'
 @app.route("/", methods=['GET','POST'])
 def home():
     if request.method == 'POST':
-        gre = request.form['gre']
         gre_verbal = request.form['gre_verbal']
+        gre_quantitative = request.form['gre_quantitative']
+        gre_writing = request.form['gre_writing']
+        gpa = request.form['gpa']
+        cost = request.form['cost']
+        pub_or_priv = request.form['pub_or_priv']
+        size = request.form['size']
+        location = request.form['location']
 
-        return render_template('results.html',
-                               result=gre, gre_verbal=gre_verbal)
+        return render_template('results.html', gre_verbal=gre_verbal, 
+        gre_quantitative=gre_quantitative, gre_writing=gre_writing, gpa=gpa, 
+        cost=cost, pub_or_priv=pub_or_priv, size=size, location=location)
     else:   
         return render_template('home.html')
 
@@ -19,7 +26,7 @@ def home():
 
 def results():
     result = 'nothing submitted'
-    return render_template("results.html", result = result)
+    return render_template("results.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
